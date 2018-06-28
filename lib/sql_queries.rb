da6468cb1093b888780afc85dc730f80767c3a0e
Brennan Fulmer <brennan.flmr@gmail.com>
2018-06-28 04:20:44 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -34,7 +34,7 @@ ORDER BY SUM(pledges.amount);"
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
   "SELECT users.name, SUM(pledges.amount) AS total_pledged
   FROM users
-  LEFT OUTER JOIN pledges
+  JOIN pledges
   ON pledges.user_id = users.id
   GROUP BY users.name
   ORDER BY total_pledged ASC, users.name"
