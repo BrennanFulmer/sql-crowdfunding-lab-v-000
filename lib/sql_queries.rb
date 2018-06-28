a5867c58f113723349ab8bf5811768f80c1c8ab4
Brennan Fulmer <brennan.flmr@gmail.com>
2018-06-28 04:18:49 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -22,7 +22,7 @@ GROUP BY projects.title
 HAVING amount_left >= 0;"
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT projects.title, SUM(pledges.amount) - projects.funding_goal
+  "SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) AS remaining
   FROM projects
   JOIN pledges
   ON pledges.project_id = projects.id
